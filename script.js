819bf28529bb792c1e36c3dcc58e6c9a328e3498
Gatera <vgatera@gmail.com>
2024-07-20 19:22:22 +0200

    updated if else statement

--- script.js
+++ script.js
@@ -11,29 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         if (taskText === "") {
             alert('Please enter a task');
             return;
-        } 
+        } else {
+            //Create new li element and set its contact to taskText
+            const taskItem = document.createElement('li');
+            taskItem.textContent = taskText;
 
-        //Create new li element and set its contact to taskText
-        const taskItem = document.createElement('li');
-        taskItem.textContent = taskText;
+            //Create button to remove task
+            const removeButton = document.createElement('button');
+            removeButton.textContent = 'Remove';
+            removeButton.className = 'remove-btn';
 
-        //Create button to remove task
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.className = 'remove-btn';
+            //Assign onclick to remove button to remove li element from taskList
+            removeButton.onclick = function() {
+                taskList.removeChild(taskItem);
+            }
 
-        //Assign onclick to remove button to remove li element from taskList
-        removeButton.onclick = function() {
-            taskList.removeChild(taskItem);
+            //append button
+            taskItem.append(removeButton);
+            //append task item
+            taskList.appendChild(taskItem);
+
+            //clear the task input
+            taskInput.value = "";
         }
-
-        //append button
-        taskItem.append(removeButton);
-        //append task item
-        taskList.appendChild(taskItem);
-
-        //clear the task input
-        taskInput.value = "";
     }
 
     addButton.addEventListener('click', addTask);
